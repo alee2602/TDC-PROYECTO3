0f2cb668414c04413e547bea1a217d706ed201ea
Derek Arreaga <drk.fab@gmail.com>
2024-11-18 19:16:12 -0600

    altering machine and simple menu on main

--- main.py
+++ main.py
@@ -2,15 +2,30 @@ from turing_machine import TuringMachine
 from yaml_loader import load_yaml
 
 def main():
-    palindrome_config = load_yaml('machines/recognitionconfig.yaml')
-    tm_palindrome = TuringMachine(palindrome_config)
 
-    print("Testing Palindrome Turing Machine...")
-    for test_input in palindrome_config['simulation_strings']:
-        print(f"\nTesting input: {test_input}")
-        tm_palindrome.load_tape(test_input)
-        tm_palindrome.execute()
-        tm_palindrome.print_tape()
+    while True:
+        print("¿Cuál Máquina de Turing deseas ejecutar?")
+        print("0. Salir")
+        print("1. Máquina de Reconocimiento")
+        print("2. Máquina de Transformación")
+        opt = input("Ingrese la opción: ")
+        if opt == "0":
+            break
+        elif opt == "1":
+            palindrome_config = load_yaml('machines/recognitionconfig.yaml')
+            tm = TuringMachine(palindrome_config)
+        elif opt == "2":
+            altering_config = load_yaml('machines/alteringconfig.yaml')
+            tm = TuringMachine(altering_config)
+        else:
+            print("Opción no válida")
+
+        print("Testing Palindrome Turing Machine...")
+        for test_input in palindrome_config['simulation_strings']:
+            print(f"\nTesting input: {test_input}")
+            tm.load_tape(test_input)
+            tm.execute()
+            tm.print_tape()
 
 if __name__ == "__main__":
     main()
--- tape.py
+++ tape.py
@@ -1,16 +1,24 @@
 class Tape:
     def __init__(self, input_string):
-        self.tape = list(input_string) + ['_'] * 100  #blanks
-        self.head = 0  #posición inicial del lector
-
+        self.tape = list(input_string)
+        self.head = 0
+    
     def read(self):
+        if self.head < 0 or self.head >= len(self.tape):
+            return '_'
         return self.tape[self.head]
 
     def write(self, symbol):
+        if self.head < 0:
+            self.tape = ['_'] * abs(self.head) + self.tape
+            self.head = 0 
+        elif self.head >= len(self.tape):
+            self.tape += ['_'] * (self.head - len(self.tape) + 1)
+        
         self.tape[self.head] = symbol
 
     def move(self, direction):
         if direction == 'L':
-            self.head = max(0, self.head - 1)
+            self.head -= 1
         elif direction == 'R':
             self.head += 1
